930e929bd7361b9e15152885e0e50789d6ceac7c
ngocminh76 <83106142+ngocminh76@users.noreply.github.com>
2025-05-27 06:10:28 +0700

    three

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,42 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
 
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Layout from './layout';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout/>,
+    children:[
+      {
+    path: "/book",
+    element: <div>Book page</div>,
+  },
+  {
+    path: "/about",
+    element: <div>about page</div>,
+  },
+    
+    ]
+  },
+  {
+    path: "/login",
+    element: <div>login page</div>,
+  },
+  {
+    path: "/register",
+    element: <div>register page</div>,
+  },
+   
+]);
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    {/* <Layout /> */}
+        <RouterProvider router={router} />
+
   </StrictMode>,
 )
